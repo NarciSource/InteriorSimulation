f397142485d65588a60ef1bdb1acca6818f7ab02
금준호 <iubns@naver.com>
2018-05-26 18:39:01 +0900

    벽붙기 작업중

--- selfInteriorSimulation/selfInteriorSimulation/Objects/AttachObject.cs
+++ selfInteriorSimulation/selfInteriorSimulation/Objects/AttachObject.cs
@@ -15,5 +15,28 @@ namespace selfInteriorSimulation
         }
 
         PointCollection points = new PointCollection();
+        public override void setPosition(Point point)
+        {
+            base.setPosition(point);
+            points.Clear();
+            points.Add(point);
+            points.Add(new Point(point.X, point.Y - Height));
+            points.Add(new Point(point.X - Width , point.Y - Height));
+            points.Add(new Point(point.X - Width, point.Y));
+
+            foreach (Point vertexPoint in points)
+            {
+                foreach (Wall wall in BasicObject.walls)
+                {
+                    for (int i = 0; i< wall.points.Count-1; i++)
+                    {
+                        int dx = Math.Abs((int)(wall.points[i].X - wall.points[i + 1].X));
+                        int dy = Math.Abs((int)(wall.points[i].Y - wall.points[i+1].Y));
+                        double r = dy / dx;
+                        int d = (int) (point.Y - (r * wall.points[i].X));
+                    }
+                }
+            }
+        }
     }
 }
