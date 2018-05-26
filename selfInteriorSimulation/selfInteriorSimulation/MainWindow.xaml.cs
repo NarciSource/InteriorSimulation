65a13536432584c14ce79f819c279fd2b0077907
금준호 <iubns@naver.com>
2018-05-27 00:49:47 +0900

    벽 붙기 추가

--- selfInteriorSimulation/selfInteriorSimulation/MainWindow.xaml.cs
+++ selfInteriorSimulation/selfInteriorSimulation/MainWindow.xaml.cs
@@ -57,7 +57,7 @@ namespace selfInteriorSimulation
 
         private void Active(object sender)
         {
-            MessageBox.Show(sender.ToString());
+            //MessageBox.Show(sender.ToString());
         }
 
 
