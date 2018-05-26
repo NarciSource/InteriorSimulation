d951cffe99b17c4e1c138f274c9d875f99a6cf1e
금준호 <iubns@naver.com>
2018-05-26 17:11:50 +0900

    의자, 소파 추가

---
+++ selfInteriorSimulation/selfInteriorSimulation/Objects/Sofa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace selfInteriorSimulation.Objects
+{
+    class Sofa : InteriorObject
+    {
+        private Sofa(Point point) : base(point)
+        {
+            setImg("sofa.PNG");
+        }
+    }
+}
